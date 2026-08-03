db6ec9ebaf2b36d057ea42de2864dd09612e3099
Abhinav Pappu <abhinav.pappu05@gmail.com>
2026-08-03 01:28:39 -0400

    refactor into one method for now

--- src/controllers/music.controller.ts
+++ src/controllers/music.controller.ts
@@ -1,26 +1,28 @@
 import type { Request, Response } from "express";
 
+type topItem = "artists" | "tracks";
+
 // Both fetch directly from the Spotify API
-export async function getTopSongs(req: Request, res: Response) {
-  const auth_token: string = req.cookies.spotify_access_token;
-  console.log(auth_token);
-  if (!auth_token) {
-    throw new Error("No Auth Token login with /spotify/login");
-  }
+function getTopItems(itemType: topItem) {
+  return async (req: Request, res: Response) => {
+    const auth_token: string = req.cookies.spotify_access_token;
+    if (!auth_token) {
+      throw new Error("No Auth Token login with /spotify/login");
+    }
 
-  const response = await fetch(
-    "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=20&offset=0",
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${auth_token}`,
+    const response = await fetch(
+      `https://api.spotify.com/v1/me/top/${itemType}?time_range=short_term&limit=20&offset=0`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${auth_token}`,
+        },
       },
-    },
-  );
-  const json_response = await response.json(); // TODO might type this later
-  res.json(json_response)
+    );
+    const json_response = await response.json(); // TODO might type this later
+    res.json(json_response);
+  };
 }
 
-export async function getTopArtists(req: Request, res: Response) {
-  const auth_token: string = req.cookies.spotify_access_token;
-}
+export const getTopSongs =  getTopItems("tracks");
+export const getTopArtists = getTopItems("artists");
